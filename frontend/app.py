4e6319188dac9b903282b54aecb23bd37a1768c2
fabiorosss <f.fabio.rossin@gmail.com>
2024-05-30 15:23:07 +0200

    scraping

--- frontend/app.py
+++ frontend/app.py
@@ -8,10 +8,137 @@ from flask import Response
 from q import *
 from mysql.connector import Error
 import hashlib
+import base64
+import requests
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+from q import *
+
+
+
+
+
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
 app.secret_key = 'your_secret_key'
+
+
+def get_driver(dati_utente):
+    options = Options()
+    # options.add_argument('--headless')  # Esegui il browser in modalità headless
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    return driver
+
+def funzione(dati_utente):
+
+    if dati_utente['giorno_partenza'] == None and dati_utente['giorno_arrivo'] == None:
+        mese_partenza = f'2024-{dati_utente["mese_partenza"]}-01_2024-{dati_utente["mese_arrivo"]}-31'  # nel caso si effettui la ricerca per mese
+        mese_arrivo = f'2024-{dati_utente["mese_partenza"]}-01_2024-{dati_utente["mese_partenza"]}-31'
+        if dati_utente['citta_partenza'] == None and dati_utente['citta_arrivo'] == None:
+            url = f"https://www.kiwi.com/it/search/tiles/{dati_utente['paese_partenza']}/{dati_utente['paese_arrivo']}/{mese_partenza}/{mese_arrivo}"
+        elif dati_utente['citta_partenza'] == None:
+            url = f"https://www.kiwi.com/it/search/tiles/{dati_utente['paese_partenza']}/{dati_utente['citta_arrivo']}-{dati_utente['paese_arrivo']}/{mese_partenza}/{mese_arrivo}"
+        elif dati_utente['citta_arrivo'] == None:
+            url = f"https://www.kiwi.com/it/search/tiles/{dati_utente['citta_partenza']}-{dati_utente['paese_partenza']}/{dati_utente['paese_arrivo']}/{mese_partenza}/{mese_arrivo}"
+        else:
+            url = f"https://www.kiwi.com/it/search/tiles/{dati_utente['citta_partenza']}-{dati_utente['paese_partenza']}/{dati_utente['citta_arrivo']}-{dati_utente['paese_arrivo']}/{mese_partenza}/{mese_arrivo}"
+
+    elif dati_utente['giorno_partenza'] == None:
+        mese_partenza = f'2024-{dati_utente["mese_partenza"]}-01_2024-{dati_utente["mese_arrivo"]}-31'  # nel caso si effettui la ricerca per mese
+        mese_arrivo = f'2024-{dati_utente["mese_arrivo"]}-{dati_utente['giorno_arrivo']}'
+        if dati_utente['citta_partenza'] == None and dati_utente['citta_arrivo'] == None:
+            url = f"https://www.kiwi.com/it/search/tiles/{dati_utente['paese_partenza']}/{dati_utente['paese_arrivo']}/{mese_partenza}/{mese_arrivo}"
+        elif dati_utente['citta_partenza'] == None:
+            url = f"https://www.kiwi.com/it/search/tiles/{dati_utente['paese_partenza']}/{dati_utente['citta_arrivo']}-{dati_utente['paese_arrivo']}/{mese_partenza}/{mese_arrivo}"
+        elif dati_utente['citta_arrivo'] == None:
+            url = f"https://www.kiwi.com/it/search/tiles/{dati_utente['citta_partenza']}-{dati_utente['paese_partenza']}/{dati_utente['paese_arrivo']}/{mese_partenza}/{mese_arrivo}"
+        else:
+            url = f"https://www.kiwi.com/it/search/tiles/{dati_utente['citta_partenza']}-{dati_utente['paese_partenza']}/{dati_utente['citta_arrivo']}-{dati_utente['paese_arrivo']}/{mese_partenza}/{mese_arrivo}"
+
+    elif dati_utente['giorno_arrivo'] == None:
+        mese_partenza = f'2024-{dati_utente["mese_partenza"]}-{dati_utente['giorno_partenza']}'
+        mese_arrivo = f'2024-{dati_utente["mese_partenza"]}-01_2024-{dati_utente["mese_arrivo"]}-31'
+        if dati_utente['citta_partenza'] == None and dati_utente['citta_arrivo'] == None:
+            url = f"https://www.kiwi.com/it/search/tiles/{dati_utente['paese_partenza']}/{dati_utente['paese_arrivo']}/{mese_partenza}/{mese_arrivo}"
+        elif dati_utente['citta_partenza'] == None:
+            url = f"https://www.kiwi.com/it/search/tiles/{dati_utente['paese_partenza']}/{dati_utente['citta_arrivo']}-{dati_utente['paese_arrivo']}/{mese_partenza}/{mese_arrivo}"
+        elif dati_utente['citta_arrivo'] == None:
+            url = f"https://www.kiwi.com/it/search/tiles/{dati_utente['citta_partenza']}-{dati_utente['paese_partenza']}/{dati_utente['paese_arrivo']}/{mese_partenza}/{mese_arrivo}"
+        else:
+            url = f"https://www.kiwi.com/it/search/tiles/{dati_utente['citta_partenza']}-{dati_utente['paese_partenza']}/{dati_utente['citta_arrivo']}-{dati_utente['paese_arrivo']}/{mese_partenza}/{mese_arrivo}"
+
+    else:
+        mese_partenza = f'2024-{dati_utente["mese_partenza"]}-{dati_utente['giorno_partenza']}'
+        mese_arrivo = f'2024-{dati_utente["mese_arrivo"]}-{dati_utente['giorno_arrivo']}'
+        if dati_utente['citta_partenza'] == None and dati_utente['citta_arrivo'] == None:
+            url = f"https://www.kiwi.com/it/search/tiles/{dati_utente['paese_partenza']}/{dati_utente['paese_arrivo']}/{mese_partenza}/{mese_arrivo}"
+        elif dati_utente['citta_partenza'] == None:
+            url = f"https://www.kiwi.com/it/search/tiles/{dati_utente['paese_partenza']}/{dati_utente['citta_arrivo']}-{dati_utente['paese_arrivo']}/{mese_partenza}/{mese_arrivo}"
+        elif dati_utente['citta_arrivo'] == None:
+            url = f"https://www.kiwi.com/it/search/tiles/{dati_utente['citta_partenza']}-{dati_utente['paese_partenza']}/{dati_utente['paese_arrivo']}/{mese_partenza}/{mese_arrivo}"
+        else:
+            url = f"https://www.kiwi.com/it/search/tiles/{dati_utente['citta_partenza']}-{dati_utente['paese_partenza']}/{dati_utente['citta_arrivo']}-{dati_utente['paese_arrivo']}/{mese_partenza}/{mese_arrivo}"
+
+    print(url)
+
+    # Ottieni il driver
+    driver = get_driver()
+
+    # Effettua la richiesta
+    driver.get(url)
+    driver.maximize_window()
+
+    input("Vai avanti")
+
+    html_content = driver.page_source
+
+    try:
+        accept_button = driver.find_element(By.ID, "cookies_accept")
+        accept_button.click()
+    except Exception as e:
+        print("Non è stato possibile trovare il bottone di accettazione dei cookie:", e)
+
+    tag_name = "span"
+    elements = driver.find_elements(By.TAG_NAME, tag_name)
+    lista_arrivi = []
+    if elements:
+        for index, element in enumerate(elements):
+            t_element = element.text
+            print(t_element)
+            if t_element.isalpha() and t_element != 'Feedback':
+                lista_arrivi.append(t_element)
+            elif '€' in t_element:
+                lista_arrivi.append(t_element.replace(' ', ''))
+    else:
+        print('nessun elemento trovato')
+
+    lista_finale = []
+    for i in range(1, len(lista_arrivi), 1):
+        if arrivo.title() in lista_arrivi[0]:
+            lista_finale.append(lista_arrivi[0])
+        if lista_arrivi[i].isalpha() and '€' in lista_arrivi[i - 1]:
+            lista_finale.append(lista_arrivi[i])
+        elif lista_arrivi[i].isalpha() and '€' in lista_arrivi[i - 1]:
+            lista_finale.append(lista_arrivi[i])
+        elif '€' in lista_arrivi[i] and lista_arrivi[i - 1].isalpha:
+            lista_finale.append(lista_arrivi[i])
+        else:
+            continue
+
+    print(lista_finale)
+    input()
+
+
 
 
 def caricamento_lista(connection, query, d):
@@ -175,17 +302,29 @@ def prezzi_voli():
     return render_template('prezzivoli.html')
 
 
-@app.route('/suggestions', methods=['GET'])
-def suggestions():
-    query = request.args.get('q')
-    connection = create_db_connection()
-    cursor = connection.cursor()
-    cursor.execute("SELECT city_name FROM cities WHERE city_name LIKE %s LIMIT 3", (f"%{query}%",))
-    results = cursor.fetchall()
-    suggestions = [row[0] for row in results]
-    cursor.close()
-    connection.close()
-    return jsonify(suggestions)
+@app.route('/ricerca', methods=['POST', 'GET'])
+def ricerca():
+    if request.method == 'POST':
+        paese_partenza = request.form.get('paese_partenza')
+        paese_arrivo = request.form.get('paese_arrivo')
+        citta_partenza = request.form.get('citta_partenza')
+        citta_arrivo = request.form.get('citta_arrivo')
+        mese_partenza = request.form.get('mese_partenza')
+        mese_arrivo = request.form.get('mese_arrivo')
+        giorno_partenza = request.form.get('giorno_partenza')
+        giorno_arrivo = request.form.get('giorno_arrivo')
+        dati = {
+            "paese_partenza": paese_partenza,
+            "paese_arrivo": paese_arrivo,
+            "citta_partenza": citta_partenza,
+            "citta_arrivo": citta_arrivo,
+            "mese_partenza": mese_partenza,
+            "mese_arrivo": mese_arrivo,
+            "giorno_partenza": giorno_partenza,
+            "giorno_arrivo": giorno_arrivo
+        }
+        if giorno_partenza is None or giorno_arrivo is None:
+            funzione_mese(dati)
 
 
 if __name__ == '__main__':
--- frontend/config.py
+++ frontend/config.py
@@ -1,5 +1,10 @@
 class Config:
+    # MYSQL_HOST = 'localhost'
+    # MYSQL_USER = 'root'
+    # MYSQL_PASSWORD = 'Gondoliere01_'
+    # MYSQL_DB = 'jetair'
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'Gondoliere01_'
+    # MYSQL_PASSWORD = 'Gondoliere01_'
+    MYSQL_PASSWORD = 'SQL46lock?'
     MYSQL_DB = 'jetair'
